45a3157e0796c2b79b2aab891089d31daafbc0a3
Jay Smit <jaytyu@gmail.com>
2019-12-04 20:24:16 +0100

    bugfix

--- main.cpp
+++ main.cpp
@@ -1,29 +1,32 @@
 #include <iostream>
+#include <cstring>
 
 void parse_arguments(int &argc, const char* argv[]){
     if (argc < 2){
         std::cout << "not enough pylons\n";
         exit(1);
     }
-    // else
-    //     std::cout<<argv[1]<<std::endl;
 
-
-    
-    if (*argv[1] == "client"){
+    if (std::strcmp(argv[1] , "client")== 0){
+        std::cout << "client jdfsjf\n";
         //something client related
         return;
     }
-    else if (*argv[1] == "server"){
-        //somthing serverlike
-        std::cout << "SERVER related stuff booting\n";
-        return;
+    else
+    {
+	    std::cout << "err\n";
+	    std::cout << argv[1];
     }
-    else{
-    std::cout << "Input is onzim\n";
-    exit(1);
-    }
-    
+    //else if (*argv[1] == "server"){
+    //    //somthing serverlike
+    //    std::cout << "SERVER related stuff booting\n";
+    //    return;
+    //}
+    //else{
+    //std::cout << "Input is onzim\n";
+    //exit(1);
+    //}
+
 }
 
 int main(int argc, char const *argv[])
